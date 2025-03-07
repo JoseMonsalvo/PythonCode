cd4c277e6a6f429df62dc6a1b0bd45a1c623f9d2
JoseMonsalvo <32096583+JoseMonsalvo@users.noreply.github.com>
2025-03-07 17:02:11 +0100

    Add files via upload

---
+++ EjerciciosPython/Ejercicio1.py
@@ -0,0 +1,108 @@
+## Ejercicio 1
+## print('Me encanta estudiar Python')
+
+
+## Ejercicio 2
+## print('Estudiar "Python" es muy divertido')
+
+## Ejercicio 3
+## print(554+1)
+
+## Ejercicio 4
+## print('Hola Mundo1\nHola Mundo2\nHola Mundo3')
+
+## Ejercicio 5
+## print('A\tB\tC\nD\tE\tF\nG\tH\tI')
+
+## Ejercicio 6
+## print('Barra normal: C:\\nBarra invertida: \\\nComillas simples: \'\nComillas dobles: \"')
+
+## Ejercicio 7
+## print(input('¿Cuál es tu nombre? '))
+
+## Ejercicio 8
+## print(input('¿Donde vives? '))
+
+## Ejercicio 9
+## print(input('Escribe tu nombre: ') + ' ' + input('Escribe tu primer apellido:') + ' ' + input('Escribe tu segundo apellido:'))
+
+## Ejercicio 10
+## number = 20
+## name = "Jose"
+
+## Ejercicio 11
+## nombre = 'Jose'
+## apellido = 'Monsalvo'
+## edad = 20
+## print('Hola, me llamo ' + nombre + ' ' + apellido + ' y tengo ' + str(edad) + ' años')
+
+## Ejercicio 12
+## curso = 'Python'
+## print('Estoy estudiando ' + curso)
+
+## Ejercicio 13
+## numEntero = 10
+## print(type(numEntero))
+
+## Ejercicio 14
+## num_decimal = 3.1416
+##print(type(num_decimal))
+
+## Ejercicio 15
+## num1 = 7.5
+## num2 = 2.5
+## num3 = num1 + num2
+## print(type (num3))
+
+## Ejercicio 16
+## num1 = 10.5
+## print(type (num1))
+## print(type(int(num1)))
+
+## Ejercicio 17
+## num2 = 10
+## print(type(float(num2)))
+
+## Ejercicio 18
+## num1 = 3.5
+## num2 = 2
+## print(float(num1) + int(num2))
+## print(type(float(num1) + int(num2)))
+
+## Ejercicio 19
+## nombreAsociado = 'Jose'
+## numeroAsociado = 12345
+## print('Estimado ' + nombreAsociado + ', gracias por su compra. Su número de pedido es ' + str(numeroAsociado))
+## print('Estimado {}, gracias por su compra. Su número de pedido es {}'.format(nombreAsociado, numeroAsociado))
+## print(f'Estimado {nombreAsociado}, gracias por su compra. Su número de pedido es {numeroAsociado}')
+
+## Ejercicio 20
+## puntosGanados = 350
+## puntosTotales = 700
+## print('Felicidades! Has ganado ' + str(puntosGanados) + ' puntos. Ahora tienes un total de ' + str(puntosTotales) + ' puntos')
+## print('Felicidades! Has ganado {} puntos. Ahora tienes un total de {} puntos'.format(puntosGanados, puntosTotales))
+## print(f'Felicidades! Has ganado {puntosGanados} puntos. Ahora tienes un total de {puntosTotales} puntos')
+
+## Ejercicio 21
+## puntosAnteriores = 300
+## puntosNuevos = 100
+## print('Felicidades! Has ganado ' + str(puntosNuevos) + ' puntos. Ahora tienes ' + str(puntosNuevos+puntosAnteriores) + ' puntos')
+## print('Felicidades! Has ganado {} puntos. Ahora tienes {} puntos'.format(puntosNuevos, puntosNuevos+puntosAnteriores))
+## print(f'Felicidades! Has ganado {puntosNuevos} puntos. Ahora tienes {puntosNuevos+puntosAnteriores} puntos')
+
+## Ejercicio 22
+
+## dividendo = 70
+## divisor = 10
+## print(str(dividendo) + ' / ' + str(divisor) + ' = ' + str(dividendo/divisor))
+## print('{} / {} = {}'.format(dividendo, divisor, dividendo/divisor))
+## print(f'{dividendo} / {divisor} = {dividendo/divisor}')
+
+## Ejercicio 23
+## num1 = 301
+## num2 = 150
+## print(str(num1) + ' % ' + str(num2) + ' = ' + str(num1 % num2))
+
+## Ejercicio 24
+num = 753
+print()
